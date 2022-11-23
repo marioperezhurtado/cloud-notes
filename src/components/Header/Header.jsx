ae941e7023b7fa478879dfea4fefb1d7339b2338
Mario Pérez Hurtado <marioph10@gmail.com>
2022-11-23 09:10:29 +0100

    Added back to my notes button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,30 @@
 import useAuth from '../../contexts/AuthContext'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 import styles from './Header.module.scss'
 
 const Header = () => {
   const navigate = useNavigate()
+  const path = useLocation()
+  const onProfile = path.pathname === '/profile'
+
   const { currentUser, logout } = useAuth()
 
   const goToProfileHandler = () => navigate('/profile')
+  const goToNotesHandler = () => navigate('/notes')
+
+  if (onProfile) {
+    return (
+      <header className={styles.header}>
+        <button className="btn btn-primary" onClick={goToNotesHandler}>
+          My Notes
+        </button>
+        <button className="btn btn-secondary" onClick={logout}>
+          Log Out
+        </button>
+      </header>
+    )
+  }
 
   return (
     <header className={styles.header}>
